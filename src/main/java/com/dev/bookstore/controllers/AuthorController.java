4c1ee15b50d9b233a8bbffa6c32247a99edf249e
hungpb13 <hungpb13@gmail.com>
2025-05-13 14:40:03 +0700

    Test Author Create Endpoint

--- src/main/java/com/dev/bookstore/controllers/AuthorController.java
+++ src/main/java/com/dev/bookstore/controllers/AuthorController.java
@@ -5,6 +5,8 @@ import com.dev.bookstore.domain.entities.Author;
 import com.dev.bookstore.mappers.impl.AuthorMapper;
 import com.dev.bookstore.services.AuthorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +22,10 @@ public class AuthorController {
     private final AuthorMapper authorMapper;
 
     @PostMapping
-    public AuthorDto createAuthor(@RequestBody AuthorDto authorDto) {
+    public ResponseEntity<AuthorDto> createAuthor(@RequestBody AuthorDto authorDto) {
         Author authorToCreate = authorMapper.toEntity(authorDto);
         Author createdAuthor = authorService.createAuthor(authorToCreate);
-        return authorMapper.toDto(createdAuthor);
+        AuthorDto createdAuthorDto = authorMapper.toDto(createdAuthor);
+        return new ResponseEntity<>(createdAuthorDto, HttpStatus.CREATED);
     }
 }
